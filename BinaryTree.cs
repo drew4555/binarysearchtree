89353c5563cc919c450f761b80058c867817a43f
andrew carr <drew4555@yahoo.com>
2019-05-20 15:29:10 -0500

    created node class and wrote logic to check if number is higher or lower than root

---
+++ BinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bionary_Search_Tree
+{
+    class BinaryTree
+    {
+        public Node root;
+        int count;
+        public BinaryTree()
+        {
+            root = null;
+            count = 0;
+        }
+        public bool IsEmpty()
+        {
+            return root == null;
+        }
+        public void Insert(int number)
+        {
+            if (IsEmpty())
+            {
+                root = new Node();
+            }
+        }
+    }
+}
---
+++ searchTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    public class Node
+    {
+        //member variables
+        public int number;
+        public Node right;
+        public Node left;
+
+        //ctor
+        public Node(int value)
+        {
+            number = value;
+            right = null;
+            left = null;
+        }
+
+        //member methods
+        public bool IsLeaf(ref Node node)
+        {
+            return (node.right == null && node.left == null);
+        }
+        public void InsertData(ref Node node, int data)
+        {
+            if(node == null)
+            {
+                node = new Node(data);
+            }
+            else if (node.number < data)
+            {
+                InsertData(ref node.right, data);
+            }
+            else if (node.number > data)
+            {
+                InsertData(ref node.left, data);
+            }
+        }
+    }
+}
